91db44e7925e8c0395bb358c60de67c7f1abb5ca
Yichun Liu <yl6343@nyu.com>
2020-08-13 21:55:43 -0400

    update by Yichun

--- StringUtilities.java
+++ StringUtilities.java
@@ -5,7 +5,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return baseValue+valueToBeAdded;
     }
 
     /**
@@ -22,7 +22,16 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        int i = 0, j = valueToBeReversed.length() - 1;
+        char[] array = valueToBeReversed.toCharArray();
+        while (i < j) {
+        	char temp = array[i];
+        	array[i]= array[j];
+        	array[j]= temp;
+        	i++;
+        	j--;
+        }
+        return new String(array);
     }
 
     /**
@@ -30,7 +39,8 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        int middleIndex = word.length() / 2;
+	return word.charAt(middleIndex);
     }
 
     /**
@@ -39,7 +49,12 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-        return null;
+        StringBuilder sb =  new StringBuilder();
+        for (char eachChar : value.toCharArray()) {
+        	if (eachChar == charToRemove) continue;
+        	sb.append(eachChar);
+        }
+        return new String (sb.toString());
     }
 
     /**
@@ -47,6 +62,7 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        String[] list  = sentence.split(" ");
+        return list[list.length - 1];
     }
 }
