9c90b7fe5958929c64b7dad7d0698224ffe1965f
sukeyzhou <luchang1991@gmail.com>
2020-08-13 21:40:08 -0400

    completed day4 assignment.

--- StringUtilities.java
+++ StringUtilities.java
@@ -5,7 +5,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return baseValue + valueToBeAdded;
     }
 
     /**
@@ -22,7 +22,11 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String res = "";
+        for (int i = valueToBeReversed.length() - 1; i >= 0; i--) {
+            res += valueToBeReversed.charAt(i);
+        }
+        return res;
     }
 
     /**
@@ -30,7 +34,8 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        int middle = word.length() / 2;
+        return word.charAt(middle);
     }
 
     /**
@@ -39,7 +44,16 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-        return null;
+        String res = "";
+        
+        for (int i = 0; i < value.length(); i++) {
+            Character c = value.charAt(i);
+            if (c != charToRemove) {
+                res += c;
+            }
+        }
+        
+        return res;
     }
 
     /**
@@ -47,6 +61,6 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        return sentence.substring(sentence.lastIndexOf(" ")+1);
     }
 }
